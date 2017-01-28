f70aad23556903231528c7fed6fca1afaa42d781
Mark Fickett <mark.fickett@gmail.com>
2017-01-28 15:42:22 -0500

    Add encryption test.

--- xor_crypt/xor_crypt_test.go
+++ xor_crypt/xor_crypt_test.go
@@ -5,6 +5,7 @@ import "strings"
 import "testing"
 
 import "../encodings"
+import "../operations"
 
 func TestDecryptSingleByteXor(t *testing.T) {
   cipher_text := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e" +
@@ -28,5 +29,24 @@ func TestScore(t *testing.T) {
     t.Error(fmt.Sprintf(
         "bad %q scored %d >= good %q which scored %d",
         bad_score, non_english, good_score, english))
+  }
+}
+
+
+func TestEncrypt(t *testing.T) {
+  clear_text :=
+      "Burning 'em, if you ain't quick and nimble\n" +
+      "I go crazy when I hear a cymbal"
+  expected_hex :=
+      "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a262263242727" +
+      "65272a282b2f20430a652e2c652a3124333a653e2b2027630c692b2028316528632630" +
+      "2e27282f"
+  key := "ICE"
+  cipher_text := operations.Xor([]byte(key), []byte(clear_text))
+  cipher_hex := encodings.EncodeHex(cipher_text)
+  if cipher_hex != expected_hex {
+    t.Error(fmt.Sprintf(
+        "%q ^ %q encrypted as %q, should be %q",
+        key, clear_text, cipher_hex, expected_hex))
   }
 }
