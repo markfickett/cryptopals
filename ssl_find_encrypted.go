92203ae2af429b6e8f3cb02359419aea1c14a11d
Mark Fickett <mark.fickett@gmail.com>
2017-02-12 16:02:13 -0500

    Add script to use Hamming distance to detect AES ECB encrypted text.

---
+++ ssl_find_encrypted.go
@@ -0,0 +1,42 @@
+/**
+ * Find which byte block is ECB-encrypted.
+ * https://cryptopals.com/sets/1/challenges/8
+ */
+
+package main
+
+import "bufio"
+import "log"
+import "math"
+import "os"
+
+import "./blocks"
+
+
+func main() {
+  if len(os.Args) > 1 {
+    log.Fatalf("Usage: %s < input_base64.txt]", os.Args[0])
+  }
+
+  scanner := bufio.NewScanner(os.Stdin)
+  line_num := 1
+  min_dist := math.Inf(1)
+  min_line := -1
+  min_cipher_text := ""
+  for scanner.Scan() {
+    data_buf := blocks.FromHex(scanner.Text())
+    dist := data_buf.GetAverageHammingDistance()
+    annotation := ""
+    if dist < min_dist {
+      min_dist = dist
+      annotation = " *"
+      min_line = line_num
+      min_cipher_text = data_buf.ToBase64()
+    }
+    log.Printf("line %d\tavg distance %f%s", line_num, dist, annotation)
+    line_num++
+  }
+  log.Printf(
+      "Line %d had minimum inter-block Hamming distance %f.\n%q\n",
+      min_line, min_dist, min_cipher_text)
+}
