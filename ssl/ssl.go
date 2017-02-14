0768bca3e956b606d8752dc0ddcff1503dfaa669
Mark Fickett <mark.fickett@gmail.com>
2017-02-14 13:30:35 -0500

    Add ECB encryption (symmetric with ECB decryption).

--- ssl/ssl.go
+++ ssl/ssl.go
@@ -24,17 +24,42 @@ func EcbDecrypt(cipher_text *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
       key.ToBytes(),
       nil)  // no initialization vector
   if err != nil {
-    panic("Unable to create context for encryption!")
+    panic("Unable to create context for decryption!")
   }
 
-  plaintext, err := ctx.DecryptUpdate(cipher_text.ToBytes())
+  clear_text, err := ctx.DecryptUpdate(cipher_text.ToBytes())
   if err != nil {
     log.Fatalf("Initial decryption failed: %q", err)
   }
-  finalplaintext, err := ctx.DecryptFinal()
+  final_clear_text, err := ctx.DecryptFinal()
   if err != nil {
     log.Fatalf("Final decryption failed: %q", err)
   }
-  plaintext = append(plaintext, finalplaintext...)
-  return blocks.FromBytes(plaintext)
+  return blocks.FromBytes(append(clear_text, final_clear_text...))
+}
+
+
+func EcbEncrypt(clear_text *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
+  cipher, err := openssl.GetCipherByName("aes-128-ecb")
+  if err != nil {
+    panic("Unable to find cipher!")
+  }
+  ctx, err := openssl.NewEncryptionCipherCtx(
+      cipher,
+      nil,  // no engine
+      key.ToBytes(),
+      nil)  // no initialization vector
+  if err != nil {
+    panic("Unable to create context for encryption!")
+  }
+
+  cipher_text, err := ctx.EncryptUpdate(clear_text.ToBytes())
+  if err != nil {
+    log.Fatalf("Initial encryption failed: %q", err)
+  }
+  final_cipher_text, err := ctx.EncryptFinal()
+  if err != nil {
+    log.Fatalf("Final encryption failed: %q", err)
+  }
+  return blocks.FromBytes(append(cipher_text, final_cipher_text...))
 }
--- ssl/ssl_test.go
+++ ssl/ssl_test.go
@@ -4,14 +4,23 @@ import "testing"
 
 import "../blocks"
 
-func TestEcbDecrypt(t *testing.T) {
-  plaintext := EcbDecrypt(
-      blocks.FromBase64("WVmOEnGj4iK3UDEZVvVYZw=="),
-      blocks.FromString("YELLOW SUBMARINE"))
-  expected_plaintext := "test\n"
-  if plaintext.ToString() != expected_plaintext {
+
+func TestEcbRoundTrip(t *testing.T) {
+  expected_clear_text := blocks.FromString("test\n")
+  expected_cipher_text := blocks.FromBase64("WVmOEnGj4iK3UDEZVvVYZw==")
+  key := blocks.FromString("YELLOW SUBMARINE")
+
+  clear_text := EcbDecrypt(expected_cipher_text, key)
+  if clear_text.ToString() != expected_clear_text.ToString() {
     t.Errorf(
         "Expected decryption as %q, but got %q.",
-        expected_plaintext, plaintext.ToString())
+        expected_clear_text.ToString(), clear_text.ToString())
+  }
+
+  cipher_text := EcbEncrypt(expected_clear_text, key)
+  if cipher_text.ToBase64() != expected_cipher_text.ToBase64() {
+    t.Errorf(
+        "Expected encryption as %q, but got %q.",
+        expected_cipher_text.ToBase64(), cipher_text.ToBase64())
   }
 }
