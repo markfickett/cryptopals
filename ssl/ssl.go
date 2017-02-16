2329a21914a4c042667aac9068d869c82f2a9d2e
Mark Fickett <mark.fickett@gmail.com>
2017-02-15 20:26:19 -0500

    Switch from third-party openssl to builtin crypto/aes library. Add CBC mode.

--- ssl/ssl.go
+++ ssl/ssl.go
@@ -1,65 +1,94 @@
 /**
- * SSL-based en/decryption.
+ * AES-based en/decryption modes.
  */
 
 package ssl
 
-import "log"
-
-// Use "go get <imported path below>" to fetch the library.
-// https://sosedoff.com/2015/05/22/data-encryption-in-go-using-openssl.html
-import "github.com/spacemonkeygo/openssl"
+import "crypto/aes"
 
 import "../blocks"
 
 
-func EcbDecrypt(cipher_text *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
-  cipher, err := openssl.GetCipherByName("aes-128-ecb")
-  if err != nil {
-    panic("Unable to find cipher!")
-  }
-  ctx, err := openssl.NewDecryptionCipherCtx(
-      cipher,
-      nil,  // no engine
-      key.ToBytes(),
-      nil)  // no initialization vector
+/**
+ * Uses the AES block cipher in ECB mode to encrypt. Pads any partial.
+ * https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+ */
+func EcbEncrypt(clear_text *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
+  cipher, err := aes.NewCipher(key.ToBytes())
   if err != nil {
     panic(err)
   }
-
-  clear_text, err := ctx.DecryptUpdate(cipher_text.ToBytes())
-  if err != nil {
-    log.Fatalf("Initial decryption failed: %q", err)
+  if clear_text.BlockSize() != aes.BlockSize {
+    panic("Block size mismatch.")
   }
-  final_clear_text, err := ctx.DecryptFinal()
-  if err != nil {
-    log.Fatalf("Final decryption failed: %q", err)
+  cipher_text := blocks.New()
+  for i := 0; i < clear_text.NumBlocks(); i++ {
+    clear_block := clear_text.BlockPadded(i).ToBytes()
+    cipher_block := make([]byte, aes.BlockSize)
+    cipher.Encrypt(cipher_block, clear_block)
+    cipher_text.AppendBytes(cipher_block)
   }
-  return blocks.FromBytes(append(clear_text, final_clear_text...))
+  return cipher_text
 }
 
 
-func EcbEncrypt(clear_text *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
-  cipher, err := openssl.GetCipherByName("aes-128-ecb")
-  if err != nil {
-    panic("Unable to find cipher!")
-  }
-  ctx, err := openssl.NewEncryptionCipherCtx(
-      cipher,
-      nil,  // no engine
-      key.ToBytes(),
-      nil)  // no initialization vector
+/**
+ * AES-decrypts blocks. Input must match AES block size and be full blocks.
+ */
+func EcbDecrypt(cipher_text *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
+  cipher, err := aes.NewCipher(key.ToBytes())
   if err != nil {
     panic(err)
   }
+  if cipher_text.BlockSize() != aes.BlockSize {
+    panic("Block size mismatch.")
+  }
+  clear_text := blocks.New()
+  for i := 0; i < cipher_text.NumBlocks(); i++ {
+    cipher_block := cipher_text.Block(i).ToBytes()
+    if len(cipher_block) < cipher_text.BlockSize() {
+      panic("Incomplete block.")
+    }
+    clear_block := make([]byte, aes.BlockSize)
+    cipher.Decrypt(clear_block, cipher_block)
+    clear_text.AppendBytes(clear_block)
+  }
+  return clear_text
+}
 
-  cipher_text, err := ctx.EncryptUpdate(clear_text.ToBytes())
-  if err != nil {
-    log.Fatalf("Initial encryption failed: %q", err)
+
+/**
+ * Encrypt using CBC mode.
+ * https://cryptopals.com/sets/2/challenges/10
+ */
+func CbcEncrypt(
+    clear_text *blocks.Blocks,
+    key *blocks.Blocks,
+    iv *blocks.Blocks) *blocks.Blocks {
+  cipher_text := blocks.New()
+  cipher_block := iv
+  for i := 0; i < clear_text.NumBlocks(); i++ {
+    clear_block := clear_text.BlockPadded(i)
+    clear_block = clear_block.Xor(cipher_block)
+    cipher_block = EcbEncrypt(clear_block, key)
+    cipher_text.Append(cipher_block)
   }
-  final_cipher_text, err := ctx.EncryptFinal()
-  if err != nil {
-    log.Fatalf("Final encryption failed: %q", err)
+  return cipher_text
+}
+
+
+func CbcDecrypt(
+    cipher_text *blocks.Blocks,
+    key *blocks.Blocks,
+    iv *blocks.Blocks) *blocks.Blocks {
+  clear_text := blocks.New()
+  prev_cipher_block := iv
+  for i := 0; i < cipher_text.NumBlocks(); i++ {
+    cipher_block := cipher_text.Block(i)
+    clear_block := EcbDecrypt(cipher_block, key)
+    clear_block = clear_block.Xor(prev_cipher_block)
+    prev_cipher_block = cipher_block
+    clear_text.Append(clear_block)
   }
-  return blocks.FromBytes(append(cipher_text, final_cipher_text...))
+  return clear_text
 }
--- ssl/ssl_test.go
+++ ssl/ssl_test.go
@@ -6,8 +6,8 @@ import "../blocks"
 
 
 func TestEcbRoundTrip(t *testing.T) {
-  expected_clear_text := blocks.FromString("test\n")
-  expected_cipher_text := blocks.FromBase64("WVmOEnGj4iK3UDEZVvVYZw==")
+  expected_clear_text := blocks.FromString("pumpkin patch U!")
+  expected_cipher_text := blocks.FromBase64("gY0wCsoqhcmxyNhqH6YE3w==")
   key := blocks.FromString("YELLOW SUBMARINE")
 
   clear_text := EcbDecrypt(expected_cipher_text, key)
@@ -22,5 +22,41 @@ func TestEcbRoundTrip(t *testing.T) {
     t.Errorf(
         "Expected encryption as %q, but got %q.",
         expected_cipher_text.ToBase64(), cipher_text.ToBase64())
+  }
+}
+
+
+func TestEcbSize(t *testing.T) {
+  in := blocks.FromString("is commonly used")
+  key := blocks.FromString("YELLOW SUBMARINE")
+  out := EcbEncrypt(in, key)
+  if in.Len() != out.Len() {
+    t.Errorf("Encryption changed length: %d to %d.", in.Len(), out.Len())
+  }
+  round_trip := EcbDecrypt(out, key)
+  if in.Len() != round_trip.Len() {
+    t.Errorf("Decryption changed length: %d to %d.", in.Len(), round_trip.Len())
+  }
+}
+
+
+func TestCbcRoundTrip(t *testing.T) {
+  iv := blocks.FromString("YELLOW SUBMARINE")
+  expected_clear_text := blocks.FromString("PUMPKIN PIE BOWL")
+  expected_cipher_text := blocks.FromBase64("oJz6RDQ/SW+QKkYsdULvcg==")
+  key := blocks.FromString("YELLOW SUBMARINE")
+
+  cipher_text := CbcEncrypt(expected_clear_text, key, iv)
+  if cipher_text.ToBase64() != expected_cipher_text.ToBase64() {
+    t.Errorf(
+        "Expected encryption as %q, but got %q.",
+        expected_cipher_text.ToBase64(), cipher_text.ToBase64())
+  }
+
+  clear_text := CbcDecrypt(cipher_text, key, iv)
+  if clear_text.ToString() != expected_clear_text.ToString() {
+    t.Errorf(
+        "Expected decryption as %q, but got %q.",
+        expected_clear_text.ToString(), clear_text.ToString())
   }
 }
