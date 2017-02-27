2a97435f21be6b114cd2de213e13a1093135a6be
Mark Fickett <mark.fickett@gmail.com>
2017-02-26 22:59:00 -0500

    Switch CBC to not depend on ECB.

--- aes_modes/aes_modes.go
+++ aes_modes/aes_modes.go
@@ -5,6 +5,7 @@
 package aes_modes
 
 import "crypto/aes"
+import "crypto/cipher"
 import "crypto/rand"
 import "fmt"
 import "log"
@@ -13,26 +14,37 @@ import "math/big"
 import "../blocks"
 
 
+func check_block_size(text *blocks.Blocks) {
+  if text.BlockSize() != aes.BlockSize {
+    panic(fmt.Sprintf(
+        "Input block size %d does not match AES block sizes %d.",
+        text.BlockSize(), aes.BlockSize))
+  }
+}
+
+
+func get_cipher(key *blocks.Blocks) cipher.Block {
+  aes_cipher, err := aes.NewCipher(key.ToBytes())
+  if err != nil {
+    panic(err)
+  }
+  return aes_cipher
+}
+
+
 /**
  * Uses the AES block cipher in ECB mode to encrypt. Pads any partial.
  * https://cryptopals.com/sets/1/challenges/7
  * https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
  */
 func EcbEncrypt(plaintext *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
-  cipher, err := aes.NewCipher(key.ToBytes())
-  if err != nil {
-    panic(err)
-  }
-  if plaintext.BlockSize() != aes.BlockSize {
-    panic(fmt.Sprintf(
-        "Plaintext block size %d does not match AES block sizes %d.",
-        plaintext.BlockSize(), aes.BlockSize))
-  }
+  aes_cipher := get_cipher(key)
+  check_block_size(plaintext)
   ciphertext := blocks.New()
   for i := 0; i < plaintext.NumBlocks(); i++ {
     plain_block := plaintext.BlockPadded(i).ToBytes()
     cipher_block := make([]byte, aes.BlockSize)
-    cipher.Encrypt(cipher_block, plain_block)
+    aes_cipher.Encrypt(cipher_block, plain_block)
     ciphertext.AppendBytes(cipher_block)
   }
   return ciphertext
@@ -43,13 +55,8 @@ func EcbEncrypt(plaintext *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
  * AES-decrypts blocks. Input must match AES block size and be full blocks.
  */
 func EcbDecrypt(ciphertext *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
-  cipher, err := aes.NewCipher(key.ToBytes())
-  if err != nil {
-    panic(err)
-  }
-  if ciphertext.BlockSize() != aes.BlockSize {
-    panic("Block size mismatch.")
-  }
+  aes_cipher := get_cipher(key)
+  check_block_size(ciphertext)
   plaintext := blocks.New()
   for i := 0; i < ciphertext.NumBlocks(); i++ {
     cipher_block := ciphertext.Block(i).ToBytes()
@@ -57,7 +64,7 @@ func EcbDecrypt(ciphertext *blocks.Blocks, key *blocks.Blocks) *blocks.Blocks {
       panic("Incomplete block.")
     }
     plain_block := make([]byte, aes.BlockSize)
-    cipher.Decrypt(plain_block, cipher_block)
+    aes_cipher.Decrypt(plain_block, cipher_block)
     plaintext.AppendBytes(plain_block)
   }
   return plaintext
@@ -72,13 +79,17 @@ func CbcEncrypt(
     plaintext *blocks.Blocks,
     key *blocks.Blocks,
     iv *blocks.Blocks) *blocks.Blocks {
+  check_block_size(plaintext)
   ciphertext := blocks.New()
-  cipher_block := iv
+  prev_cipher_block := iv
+  aes_cipher := get_cipher(key)
   for i := 0; i < plaintext.NumBlocks(); i++ {
     plain_block := plaintext.BlockPadded(i)
-    plain_block = plain_block.Xor(cipher_block)
-    cipher_block = EcbEncrypt(plain_block, key)
-    ciphertext.Append(cipher_block)
+    plain_block = plain_block.Xor(prev_cipher_block)
+    cipher_block := make([]byte, aes.BlockSize)
+    aes_cipher.Encrypt(cipher_block, plain_block.ToBytes())
+    ciphertext.AppendBytes(cipher_block)
+    prev_cipher_block = blocks.FromBytes(cipher_block)
   }
   return ciphertext
 }
@@ -88,14 +99,16 @@ func CbcDecrypt(
     ciphertext *blocks.Blocks,
     key *blocks.Blocks,
     iv *blocks.Blocks) *blocks.Blocks {
+  check_block_size(ciphertext)
   plaintext := blocks.New()
   prev_cipher_block := iv
+  aes_cipher := get_cipher(key)
   for i := 0; i < ciphertext.NumBlocks(); i++ {
     cipher_block := ciphertext.Block(i)
-    plain_block := EcbDecrypt(cipher_block, key)
-    plain_block = plain_block.Xor(prev_cipher_block)
+    plain_block := make([]byte, aes.BlockSize)
+    aes_cipher.Decrypt(plain_block, cipher_block.ToBytes())
+    plaintext.Append(blocks.FromBytes(plain_block).Xor(prev_cipher_block))
     prev_cipher_block = cipher_block
-    plaintext.Append(plain_block)
   }
   return plaintext
 }
